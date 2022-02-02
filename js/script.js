142a34a0ca012454bf3aa8418df7f9f646ae5352
ruby <betwixtbrackets@gmail.com>
2022-02-02 14:06:20 -0800

    1. accept and validate user guesses 2. check for duplicate guesses

--- js/script.js
+++ js/script.js
@@ -1,39 +1,77 @@
 // 1. create the global variables to select the elements
-const guessedLettersUL = document.querySelector(".guessed-letters");
+const guessedLettersElement = document.querySelector(".guessed-letters");
 const guessLetterButton = document.querySelector(".guess");
-const playerGuessInput = document.querySelector(".letter");
+const letterInput = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
-const remainingGuesses = document.querySelector(".remaining");
+const remainingGuessesElement = document.querySelector(".remaining");
 const remainingGuessesSpan = document.querySelector(" remaining span");
-const guessMessages = document.querySelector(".message");
+const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 // Magnolia is the starting word to test out the game
 const word = "magnolia";
+// array for all the letters guessed
+const guessedLetters = [];
 
 
-// add placeholders for characters
+// Display placeholders for the word's letters
 const addPlaceholders = function (word) {
     // set empty array
     const placeholderLetters = [];
     // set loop
     for (const letter of word) {
-        // console.log(letter);
+        console.log(letter);
         // add element to the array
         placeholderLetters.push("●");
-        // console.log(placeholderLetters);
     }
     // update the text of the selected element with array elements joined into a string
-    wordInProgress.innerText = placeholderLetters.join("");
-    
+    wordInProgress.innerText = placeholderLetters.join(""); 
 };
 addPlaceholders(word);
 
-// capture letter entered
 guessLetterButton.addEventListener("click", function (e) {
+    // Don't want the default reload behavior
     e.preventDefault();
-    // set a variable to capture user input
-    const guessInput = playerGuessInput.value;
-    console.log(guessInput);
+    // Empty message paragraph
+    message.innerText = "";
+    // SETTING USER INPUT TO VARIABLE GUESS
+    const guess = letterInput.value;
+    // Let's make sure that the input is a single letter by calling our validate function below with user input as the parameter
+    const goodGuess = validateInput(guess);
+
+    if (goodGuess) {
+        makeGuess(guess);
+    };
     // reset to empty
-    playerGuessInput.value = "";
+    letterInput.value = ""; 
 });
+
+// Let's validate input (string.length)
+const validateInput = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    // Have you entered a letter?
+    if (input.length === 0) {
+    message.innerText = "Please enter a letter.";
+    // Have you entered more than one letter?
+    } else if (input.length > 1) {
+        message.innerText = "Please enter only one letter from A to Z.";
+    } else if (!input.match(acceptedLetter)) {
+        // Have you entered a non-letter?
+        message.innerText = "Please enter a letter from A to Z.";
+    } else {
+        // input is a single letter -- yayness
+        return input;
+    }
+};
+
+// Let's test for duplicate guesses
+const makeGuess = function (guess) {
+    guess = guess.toUpperCase();
+// We've got a duplicate/it's in the array
+    if (guessedLetters.includes(guess)) {
+        message.innerText = "That letter's been guessed already."
+    } else { // Add to the array
+        guessedLetters.push(guess)
+        console.log(guessedLetters);
+    }
+};
+
