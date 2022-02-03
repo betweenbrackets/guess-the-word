8ae334fe6e39a2f03735df22e9cba36a5641ab3b
ruby <betwixtbrackets@gmail.com>
2022-02-03 13:00:27 -0800

    show the guessed letters, update the placeholders with the correct guesses, check if the player has won

--- js/script.js
+++ js/script.js
@@ -28,12 +28,14 @@ const addPlaceholders = function (word) {
 };
 addPlaceholders(word);
 
+// set button behavior
+// set user input variable
 guessLetterButton.addEventListener("click", function (e) {
     // Don't want the default reload behavior
     e.preventDefault();
     // Empty message paragraph
     message.innerText = "";
-    // SETTING USER INPUT TO VARIABLE GUESS
+    // SETTING USER INPUT TO VARIABLE "GUESS"
     const guess = letterInput.value;
     // Let's make sure that the input is a single letter by calling our validate function below with user input as the parameter
     const goodGuess = validateInput(guess);
@@ -69,9 +71,58 @@ const makeGuess = function (guess) {
 // We've got a duplicate/it's in the array
     if (guessedLetters.includes(guess)) {
         message.innerText = "That letter's been guessed already."
-    } else { // Add to the array
+    } else { // POPULATE THE gUESSEDLETTERS ARRAY
         guessedLetters.push(guess)
         console.log(guessedLetters);
+        // Call the showGuessedLetter function here so that the letter displays only if it hasn't been guessed before
+        showGuessedLetters();
+        updateWordInProgress(guessedLetters);
     }
 };
+
+// Let's display the guessed letters
+const showGuessedLetters = function () {
+    guessedLettersElement.innerHTML = "";
+    for (let guessedLetter of guessedLetters) {
+        const listItem = document.createElement("li");
+        listItem.innerText = guessedLetter;
+        guessedLettersElement.append(listItem);
+    }
+};
+
+// Let's update the Word In Progress - push either the character or the symbol
+// array as parameter
+const updateWordInProgress = function (guessedLetters) {
+    // declare word is progress variable and transform to upper case to avoid leter case errors
+    const wordUpper = word.toUpperCase();
+    // split the string and return as elements in an array
+    const wordArray = wordUpper.split("");
+    // console.log("This is " + wordArray);
+    // set the array of both correct guesses and placeholders
+    const revealWord = [];
+    // test each element of guessedLetters for a match of wordArray
+    for (const character of wordArray) {
+        if (guessedLetters.includes(character)) {
+            // if match, push the character 
+            revealWord.push(character.toUpperCase())
+        } else { // if don't match, push placeholder
+                revealWord.push("●")
+            }
+    }
+    console.log(revealWord);
+    // update the text with the new array elements
+    wordInProgress.innerText = revealWord.join("");
+    checkIfWonYet();
+};
+
+// Let's check if we've won yet
+const checkIfWonYet = function () {
+    // test the word against the guesses
+    if (word.toUpperCase() === wordInProgress.innerText) {
+        // if they match, party!
+        message.classList.add("win");
+        message.innerHTML = '<p class="highlight">You guessed correct the word! Congrats!</p>';
+    }
+};
+
 
