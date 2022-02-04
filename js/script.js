261f6338c284754a106f75227b235fad108d6dc7
ruby <betwixtbrackets@gmail.com>
2022-02-04 12:43:20 -0800

    enable play again

--- js/script.js
+++ js/script.js
@@ -4,13 +4,14 @@ const guessLetterButton = document.querySelector(".guess");
 const letterInput = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
 const remainingGuessesElement = document.querySelector(".remaining");
-const remainingGuessesSpan = document.querySelector(".remaining span");
+let remainingGuessesSpan = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
+
 // Magnolia is the starting word to test out the game
 let word = "magnolia";
 // Array for all the letters guessed
-const guessedLetters = [];
+let guessedLetters = [];
 // Set the number of guesses
 let remainingGuesses = 8;
 
@@ -157,6 +158,8 @@ const updateGuessesRemaining = function (guess) {
 // count down the remaining guesses
     if (remainingGuesses === 0) {
         message.innerHTML = `Game over! The word was <span class="highlight">${word}</span>.`;
+        remainingGuessesSpan.innerText = `${remainingGuesses} guesses`;
+        startOver();
     } else if (remainingGuesses === 1) {
         remainingGuessesSpan.innerText = `${remainingGuesses} guess`;
     } else {
@@ -171,5 +174,36 @@ const checkIfWin = function () {
         // if they match, party!
         message.classList.add("win");
         message.innerHTML = '<p class="highlight">You guessed correct the word! Congrats!</p>';
-    }
+
+        startOver();
+    } 
 };
+
+// Let's show the Play Again button
+const startOver = function () {
+    // hide the selected  elements
+    guessLetterButton.classList.add("hide");
+    remainingGuessesElement.classList.add("hide");
+    guessedLettersElement.classList.add("hide");
+    // show the Play Again button
+    playAgainButton.classList.remove("hide");
+};
+
+playAgainButton.addEventListener("click", function () {
+    // reset all original values and grab new word
+    message.classList.remove("win");
+    guessedLetters = [];
+    remainingGuesses = 8;
+    remainingGuessesSpan.innerText = `${remainingGuesses} guesses`;
+    guessedLettersElement.innerHTML = "";
+    message.innerText = "";
+    // grap the new word
+    getWord();
+
+    
+    // show the right elements
+    guessLetterButton.classList.remove("hide");
+    playAgainButton.classList.add("hide");
+    remainingGuessesElement.classList.remove("hide");
+    guessedLettersElement.classList.remove("hide");
+});
